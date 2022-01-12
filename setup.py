68cf567f9ebfd06d1b051692eb81dd64693ddd61
Erla Hrafnkelsdóttir <erlahrafnkelsdottir@Erlas-MacBook-Pro.local>
2022-01-12 22:36:16 +0100

    Make cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for course 02476 Machine Learning Operations at DTU, January 20222.',
+    author='Erla Hrafnkelsdóttir',
+    license='MIT',
+)
